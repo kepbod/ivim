823f02da67504df46df47e8c1658e5438322e03a
Huxinbang <huxinbang@gmail.com>
2014-01-22 22:23:53 +0800

    Support vim 7.4

--- tools/bootstrap.sh
+++ tools/bootstrap.sh
@@ -22,7 +22,7 @@ printf '\033[0;34m%s\033[0m\n' '                      '
 # check requriements
 printf '\033[0;34m%s\033[0m\n' "Checking requriements for ivim..."
 printf '\033[0;34m%s\033[0m\n' "Checking vim version..."
-vim --version | grep 7.3 || die "Your vim's version is too low!\nPlease download higher version(7.3+) from http://www.vim.org/download.php"
+vim --version | grep 7.4 || die "Your vim's version is too low!\nPlease download higher version(7.4+) from http://www.vim.org/download.php"
 printf '\033[0;34m%s\033[0m\n' "Checking if git exists..."
 which git || die "No git installed!\nPlease install git from http://git-scm.com/downloads/"
 printf '\033[0;34m%s\033[0m\n' "Check if ctags exists..."
